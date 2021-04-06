544310d3e2af4fa0a39a5d8640ca51577db099b0
emcog <robin.leonard@gmail.com>
2021-04-06 15:17:16 +0100

    jest testing adding accoring to https://timdeschryver.dev/blog/how-to-test-svelte-components but not working, possibly due to being sveltekit app rather than svelte app

---
+++ tests/testCounter.spec.js
@@ -0,0 +1,24 @@
+// refernece https://timdeschryver.dev/blog/how-to-test-svelte-components
+
+// import Counter from '$lib/Counter.svelte'
+import Counter from '../src/lib/Counter.svelte'
+import { render, fireEvent } from '@testing-library/svelte'
+
+
+it('it works', async () => {
+	const { getByText, getByTestId } = render(Counter)
+
+	const increment = getByText('increment')
+	const decrement = getByText('decrement')
+	const counter = getByTestId('counter-value')
+
+	await fireEvent.click(increment)
+	await fireEvent.click(increment)
+	await fireEvent.click(increment)
+	await fireEvent.click(decrement)
+
+	expect(counter.textContent).toBe('2')
+
+	// with jest-dom
+	expect(counter).toHaveTextContent('2')
+})
