c1be7125b631178d3ded39dfdcf1f9e3bff1dd6b
emcog <robin.leonard@gmail.com>
2021-08-31 22:00:38 +0100

    basic tidy up

--- .svelte-kit/dev/generated/manifest.js
+++ .svelte-kit/dev/generated/manifest.js
@@ -2,9 +2,7 @@ const c = [
 	() => import("../../../src/routes/__layout.svelte"),
 	() => import("../components/error.svelte"),
 	() => import("../../../src/routes/index.svelte"),
-	() => import("../../../src/routes/presshold.svelte"),
-	() => import("../../../src/routes/example.svelte"),
-	() => import("../../../src/routes/phab.svelte")
+	() => import("../../../src/routes/example.svelte")
 ];
 
 const d = decodeURIComponent;
@@ -13,14 +11,8 @@ export const routes = [
 	// src/routes/index.svelte
 	[/^\/$/, [c[0], c[2]], [c[1]]],
 
-	// src/routes/presshold.svelte
-	[/^\/presshold\/?$/, [c[0], c[3]], [c[1]]],
-
 	// src/routes/example.svelte
-	[/^\/example\/?$/, [c[0], c[4]], [c[1]]],
-
-	// src/routes/phab.svelte
-	[/^\/phab\/?$/, [c[0], c[5]], [c[1]]]
+	[/^\/example\/?$/, [c[0], c[3]], [c[1]]]
 ];
 
 // we import the root layout/error components eagerly, so that
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,45 +1,16 @@
 <script>
-	import Counter from '$lib/Counter.svelte';
+	import PressHoldContainer from '../lib/PressHoldContainer.svelte';
 </script>
 
-<main>
-	<h1>Hello world!</h1>
+<div class="gradient-bg fill-the-view">
+	<PressHoldContainer />
+</div>
 
-	<Counter />
+<style>
+    .gradient-bg{ background-image: linear-gradient(to bottom, #7de3a9, #5fc189 21%, #1b442d 95%); }
 
-	<p>Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style lang="scss">
-	main {
-		text-align: center;
-		padding: 1em;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4rem;
-		font-weight: 100;
-		line-height: 1.1;
-		margin: 4rem auto;
-		max-width: 14rem;
-	}
-
-	p {
-		max-width: 14rem;
-		margin: 2rem auto;
-		line-height: 1.35;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			max-width: none;
-		}
-
-		p {
-			max-width: none;
-		}
-	}
+    .fill-the-view {
+        width: 100vw;
+        height: 100vh;
+    }
 </style>
